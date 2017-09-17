7b9173bddf55695141cc10ddf0bac41eb5860a42
Pujan Thapa <pujanthapa@rocketmail.com>
2017-09-17 18:32:38 +0545

    Project 6 and 7

---
+++ euler-6.py
@@ -0,0 +1,8 @@
+def square_sum(a):
+	sum_num = a*(a+1)//2;
+	return sum_num**2
+def sum_square(a):
+	square_sum = a* (a+1)*(2*a+1)//6
+	return(square_sum)
+
+print(square_sum(100)-sum_square(100))
---
+++ euler-7.py
@@ -0,0 +1,36 @@
+#fast one
+import time
+
+def fast_nth_prime(n, limit=125000):
+    if limit % 2 != 0: limit += 1
+    primes = [True] * limit
+    primes[0],primes[1] = [None] * 2
+    count = 0 # how many primes have we found?
+    for ind,val in enumerate(primes):
+        if val is True:
+            # sieve out non-primes by multiples of known primes
+            primes[ind*2::ind] = [False] * (((limit - 1)//ind) - 1)
+            count += 1
+        if count == n: return ind
+    return False
+ 
+start = time.time()
+prime = fast_nth_prime(10001)
+elapsed = (time.time() - start)
+ 
+print("found %s in %s seconds." % (prime,elapsed))
+
+
+
+#extremely loong loong time
+def prime_nth():
+    c=[]
+    for i in range(2,125000):
+        if all(i%a!=0 for a in range(2,i)):
+            c.append(i)
+        if len(c)==10001:
+            break;
+    return c[10000]
+
+a=prime_nth()
+print(a)
