df83d0ddac2573e1c09804f1cd4d2bec17b9ac09
t_barun <pujanthapa88.63@gmail.com>
2018-11-06 13:35:26 +0545

    Project 20

---
+++ euler-20.py
@@ -0,0 +1,14 @@
+def factorial(n):
+	if n==0 or n==1:
+		return 1
+	else:
+		return n*factorial(n-1)
+
+def sum_of_digits(number):
+	fact = factorial(number)
+	return sum(map(int, str(fact)))
+
+if __name__ == "__main__":
+	num = int(input("Enter a number"))
+	result = sum_of_digits(num)
+	print(result)
