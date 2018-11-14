b5998db9dfced083c04ec797c04aa5e3d467a6ac
t_barun <pujanthapa88.63@gmail.com>
2018-11-14 13:17:08 +0545

    Project euler 21

---
+++ euler-21.py
@@ -0,0 +1,26 @@
+import time
+
+def sum_of_divisors(num):
+    return sum([fact for fact in range(1, num) if num % fact == 0])
+
+
+def amicable(n):
+    a=time.time()
+    amicables = set()
+    for num in range(1,n+1):
+     if num in amicables:
+         continue
+     sum_fact = sum_of_divisors(num)
+     sum_fact2 = sum_of_divisors(sum_fact)
+     if num == sum_fact2 and num != sum_fact:
+         amicables.add(num)
+         amicables.add(sum_fact2)
+    return (sum(amicables), time.time()-a)
+
+
+if __name__ == "__main__":
+    a = int(input("Enter a number up to which the sum of amicable numbers are needed"))
+    sum_total, time_elapsed = amicable(a)
+    print(sum_total ,time_elapsed)
+
+
