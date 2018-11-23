bc939bb6901ca44b348537a5948c44662c8f2a99
t_barun <pujanthapa88.63@gmail.com>
2018-11-23 12:45:44 +0545

    Optimized

--- euler-24.py
+++ euler-24.py
@@ -1,10 +1,17 @@
 import time
 from itertools import permutations
 
+# def find_millionth():
+# 	list_1_to_10 ='0123456789'
+# 	b=time.time()
+# 	millionth = [''.join(p) for p in permutations(list_1_to_10)][999999]
+# 	return (millionth , time.time()-b)
+
+#fast version
 def find_millionth():
-	list_1_to_10 ='0123456789'
+	list_1_to_10=[0,1,2,3,4,5,6,7,8,9]
 	b=time.time()
-	millionth = [''.join(p) for p in permutations(list_1_to_10)][999999]
+	millionth = list(permutations(list_1_to_10))[999999]                
 	return (millionth , time.time()-b)
 
 if __name__ == '__main__':
