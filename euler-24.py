a3349aedcf3a181f160f47e7c77ae6ff40e7168d
t_barun <pujanthapa88.63@gmail.com>
2018-11-23 12:44:51 +0545

    Project 24

---
+++ euler-24.py
@@ -0,0 +1,11 @@
+import time
+from itertools import permutations
+
+def find_millionth():
+	list_1_to_10 ='0123456789'
+	b=time.time()
+	millionth = [''.join(p) for p in permutations(list_1_to_10)][999999]
+	return (millionth , time.time()-b)
+
+if __name__ == '__main__':
+	print("The 1 millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9 ", find_millionth())
